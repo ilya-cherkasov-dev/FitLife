37aa854301faa74d0b3fd478c2abe8b9a419142e
Ilya Cherkasov <iliacherkasov160489@gmail.com>
2026-05-04 19:23:57 +0200

    Выполнил задание 3 урока исправил ошибки

--- fit_life.py
+++ fit_life.py
@@ -1,23 +1,35 @@
 # Проект FitLife - MVP версия 1.0
 
+WATER_PER_KG = 30
+ML_IN_LITER = 1000
+
 
 # 1. Знакомство
-user_name = input("Введите ваше имя: ")
-user_age = int(input("Введите ваш возраст: "))
+user_name = input("Введите ваше имя: ").strip()
+
+try:
+    user_age = int(input("Введите ваш возраст: "))
+except ValueError:
+    print("Ошибка: возраст должен быть числом.")
+    user_age = 0
+
 
 # 2. Сбор данных
-user_weight = float(input("Введите вес (кг): "))
-user_height = float(input("Введите рост (в метрах, например 1.75): "))
+try:
+    user_weight = float(input("Введите вес (кг): "))
+    user_height = float(input("Введите рост (в метрах, например 1.75): "))
+except ValueError:
+    print("Ошибка: вес и рост должны быть числами.")
+    user_weight = 0.0
+    user_height = 1.0
 
 
-# 3. Логика расчетов (Функции как "черный ящик": используем арифметику)
+# 3. Логика расчетов
 # Формула ИМТ: вес разделить на (рост в квадрате)
 bmi = round(user_weight / (user_height ** 2), 1)
 
-
-# Подсчет воды: вес * 30 мл
-water_ml = user_weight * 30
-water_l = water_ml / 1000
+# Подсчет воды: вес * 30 мл, переводим в литры
+water_l = (user_weight * WATER_PER_KG) / ML_IN_LITER
 
 
 # 4. Вывод красивого результата
